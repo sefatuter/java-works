a83a1189fff9f8f6123616ba5724d30ebdfdb727
sefatuter <sefa2233_18@hotmail.com>
2024-07-24 16:00:32 +0300

    Basics

--- 01-hello/src/Main.java
+++ 01-hello/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!"); // Comment line
+        System.out.println(1000);
+        System.out.println(11 * 11);
+
+        String name = "john";
+        int age = 20; // age = 11;
+        float myFloat = 5.99f;
+        boolean myBoolean = true;
+        char myChar = 'A';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(myFloat);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+        System.out.println("Hello " + name + " " + age + " " + myFloat + " " + myBoolean + " " + myChar);
     }
 }
