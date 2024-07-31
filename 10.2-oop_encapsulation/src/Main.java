54724aef1221ec66c98d3ffc048ec1b1174bbd89
sefatuter <sefa2233_18@hotmail.com>
2024-07-31 21:11:58 +0300

    encapsulation

---
+++ 10.2-oop_encapsulation/src/Main.java
@@ -0,0 +1,13 @@
+import com.sefa.model.Worker;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+
+        worker.setId(10L);
+        System.out.println("Worker id is " + worker.getId());
+        worker.setSalary(8000); // Not gonna work because of the condition
+        worker.setSalary(1500);
+        System.out.println("Worker salary is " + worker.getSalary());
+    }
+}
