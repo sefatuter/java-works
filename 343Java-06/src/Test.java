a3fd2202679df5de4dbdcdf193d1737d752eeef3
sefatuter <sefa2233_18@hotmail.com>
2024-10-03 20:46:52 +0300

    343 ch6

---
+++ 343Java-06/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    static void testmethod(){
+        System.out.println("this is static method");
+    }
+
+    void testmethod2(){
+        System.out.println("this is method2");
+    }
+}
