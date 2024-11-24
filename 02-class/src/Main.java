fb0781b4156855516313c23c77faec4510593d63
sefa <sefa2233_18@hotmail.com>
2024-11-24 16:57:33 +0300

    gui ex

--- 02-class/src/Main.java
+++ 02-class/src/Main.java
@@ -1,4 +1,10 @@
+import javax.swing.*;
+
 public class Main {
+    private JLabel jsFirstName;
+    private JPanel MyPanel;
+    private JLabel jsLabel;
+
     public static void main(String[] args) {
         Car myCar = new Car();
         myCar.brand = "Toyota";
